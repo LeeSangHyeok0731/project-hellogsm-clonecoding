47d81797d3c1404419aa7a573f3fb8351d0bd759
fakesanghyeok <s24066@gsm.hs.kr>
2024-09-11 22:56:03 +0900

    프로젝트의 시작

---
+++ src/navheader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderWrapper = styled.div`
+    padding: 0 160px 0 160px;
+    width:1200px;
+    height:74px;
+    background-color:skyblue;
+`
+
+function NavHeader(){
+    return(
+        <HeaderWrapper>
+            
+        </HeaderWrapper>
+    )
+}
